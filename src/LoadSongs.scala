373e75cd8edf748888cd815ce2a0e63d509603fe
Pratik Singh <pratik.iem01@gmail.com>
2018-05-12 01:44:42 -0400

    Added FFT

--- src/LoadSongs.scala
+++ src/LoadSongs.scala
@@ -1,10 +1,12 @@
-import java.io.{BufferedInputStream, ByteArrayOutputStream, FileInputStream}
-
+import java.io.{BufferedInputStream, ByteArrayOutputStream, FileInputStream, InputStream}
 import javax.sound.sampled.AudioSystem
 import org.apache.commons.math3.complex.Complex
 import org.apache.commons.math3.transform.{DftNormalization, FastFourierTransformer, TransformType}
+import co.theasi.plotly._
+import co.theasi.plotly.writer.Server
 
-class Audio(val data: Array[Byte], val byteFreq: Int, val sampleRate: Float, val minTime: Long, val id: Int) {
+
+class Audio(val data: Array[Byte], val byteFreq: Int, val sampleRate: Float, val minTime: Long, val id: Int = 0) {
 
   def timeDomain(): Array[(Double, Int)] = {
     data.zipWithIndex.map{ case(b, idx) => (minTime + idx * 1000L / byteFreq.toDouble, b.toInt) }
@@ -32,63 +34,78 @@ class Audio(val data: Array[Byte], val byteFreq: Int, val sampleRate: Float, val
 
 object LoadSongs {
 
-  def readFile(song:String): Unit = {
+  def readFile(song:String): Audio = {
     val is = new FileInputStream(song)
-    processSong(is)
+    processSong(is, 0, 10)
   }
 
-  def processSong(stream: FileInputStream): Unit = {
+  def processSong(stream: InputStream, minTime: Long, maxTime: Long): Audio = {
+
+    require(minTime >= 0)
+    require(minTime < maxTime)
+
     val bufferedIn = new BufferedInputStream(stream)
     val out = new ByteArrayOutputStream
     val audioInputStream = AudioSystem.getAudioInputStream(bufferedIn)
     val format = audioInputStream.getFormat
+    val sampleRate = format.getSampleRate
+    val sizeTmp = Math.rint((format.getFrameRate * format.getFrameSize) / format.getFrameRate).toInt
+    val size = (sizeTmp + format.getFrameSize) - (sizeTmp % format.getFrameSize)
+    val byteFreq: Int = format.getFrameSize * format.getFrameRate.toInt
 
-    println(format.toString)
-    // PCM_SIGNED 44100.0 Hz, 16 bit, stereo, 4 bytes/frame, little-endian
+    println("Channels " + format.getChannels)
+    println("Encoding " + format.getEncoding)
+    println("FrameRate " + format.getFrameRate)
+    println("FrameSize " + format.getFrameSize)
+    println("SampleRate " + format.getSampleRate)
+    println("SampleSizeInBits " + format.getSampleSizeInBits)
+    println("byteFreq " + byteFreq)
 
-    val tmpSize = Math.rint((format.getFrameRate * format.getFrameSize)/format.getFrameSize).toInt
-    val size = (tmpSize + format.getFrameSize) - (tmpSize % format.getFrameSize)
-    val buffer = new Array[Byte](size)
+    val buffer: Array[Byte] = new Array[Byte](size)
+    val maxLength = if (maxTime == Long.MaxValue) Long.MaxValue else byteFreq * maxTime / 1000
+    val minLength = byteFreq * minTime / 1000
 
     var available = true
-    var readCount = 0
-
-    while(available){
+    var totalRead = 0
+    while (available) {
       val c = audioInputStream.read(buffer, 0, size)
-
-      if(c > -1){
-        readCount += c
+      totalRead += c
+      if (c > -1 && totalRead >= minLength && totalRead < maxLength) {
         out.write(buffer, 0, c)
-      }else{
-        available = false
+      } else {
+        if (totalRead >= minLength) {
+          available = false
+        }
       }
     }
 
     audioInputStream.close()
     out.close()
-    out.toByteArray
+
+    new Audio(out.toByteArray, byteFreq, sampleRate, minTime)
 
   }
+
+
 
   def main(args:Array[String]): Unit ={
 
-    val audio = readFile("src/Data/mario.wav")
+    val audioObject = readFile("src/Data/mario.wav")
+    print(audioObject)
+    print(audioObject.timeDomain().length)
+    audioObject.timeDomain().filter(x => x._2>0).foreach(print)
+
+    //plot()
+
   }
 
-  def test={
+  def plot() = {
+    implicit val server = new Server {
+      val credentials = writer.Credentials("psingh22", "HblmXXPa72Kid2WN09wJ")
+      val url = "https://api.plot.ly/v2/"
+    }
 
-    val trace1 = Scatter(
-      Seq(1, 2, 3, 4),
-      Seq(10, 15, 13, 17)
-    )
-
-    val trace2 = Scatter(
-      Seq(1, 2, 3, 4),
-      Seq(16, 5, 11, 9)
-    )
-
-    val data = Seq(trace1, trace2)
-
-    Plotly.plot("div-id", data)
+    val p = Plot().withScatter(Vector(1, 2), Vector(3, 4))
+    draw(p, "custom-credentials")
   }
 }
